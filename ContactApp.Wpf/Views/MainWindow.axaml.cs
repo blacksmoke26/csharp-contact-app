3aa06b731c9466d7d7f3d22eaae900bfcd18e5d7
Junaid Atari <mj.atari@gmail.com>
2025-01-31 00:31:27 +0500

    refactor(ui): use custom tailored sidebar component

--- ContactApp.Wpf/Views/MainWindow.axaml.cs
+++ ContactApp.Wpf/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
+using System;
 using Avalonia.Controls;
-using Avalonia.Data;
+using Avalonia.Interactivity;
 using ContactApp.Wpf.Controls;
 
 namespace ContactApp.Wpf.Views;
@@ -7,5 +8,10 @@ namespace ContactApp.Wpf.Views;
 public partial class MainWindow : Window {
   public MainWindow() {
     InitializeComponent();
+  }
+
+  private void SidebarControl_OnSelectedChange(object? sender, RoutedEventArgs e) {
+    var source = (SidebarControl)e.Source!;
+    Console.WriteLine($"Selected Value changed: {source.Selected}");
   }
 }
