a014d11787505d98306f9abb155259c033839581
Junaid Atari <mj.atari@gmail.com>
2025-01-31 22:46:40 +0500

    style(views): define grid and using controls

--- ContactApp.Wpf/Views/MainWindow.axaml.cs
+++ ContactApp.Wpf/Views/MainWindow.axaml.cs
@@ -1,3 +1,7 @@
+// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
 using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
