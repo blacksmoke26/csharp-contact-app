02783e4ceb9338bf1a6398b9047368d05914cb14
Junaid Atari <mj.atari@gmail.com>
2025-01-31 22:57:32 +0500

    refactor(core): implement DI and tweak view-locator

--- ContactApp.Wpf/App.axaml.cs
+++ ContactApp.Wpf/App.axaml.cs
@@ -1,11 +1,13 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
-using System.Linq;
 using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.DependencyInjection;
 using ContactApp.Wpf.ViewModels;
+using ContactApp.Wpf.ViewModels.Forms;
 using ContactApp.Wpf.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ContactApp.Wpf;
 
@@ -15,12 +17,35 @@ public partial class App : Application {
   }
 
   public override void OnFrameworkInitializationCompleted() {
+    ViewLocator locator = new();
+    DataTemplates.Add(locator);
+
+    // configure services
+    ConfigureServices();
+
+    // Remove vanilla validation plugins
+
+    #region ValidationPlugins
+
+    // https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+
+    // Get an array of plugins to remove
+    var dataValidationPluginsToRemove =
+      BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+
+    // remove each entry found
+    foreach (var plugin in dataValidationPluginsToRemove) {
+      BindingPlugins.DataValidators.Remove(plugin);
+    }
+
+    #endregion
+
     if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
       // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
       // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
       DisableAvaloniaDataAnnotationValidation();
       desktop.MainWindow = new MainWindow {
-        DataContext = new MainWindowViewModel(),
+        DataContext = Ioc.Default.GetRequiredService<MainWindowViewModel>(),
       };
     }
 
@@ -36,5 +61,22 @@ public partial class App : Application {
     foreach (var plugin in dataValidationPluginsToRemove) {
       BindingPlugins.DataValidators.Remove(plugin);
     }
+  }
+
+  /// <summary>
+  /// Register DI services<br/>
+  /// Reference: https://learn.microsoft.com/en-us/dotnet/api/microsoft.extensions.dependencyinjection.iservicecollection
+  /// </summary>
+  private void ConfigureServices() {
+    ServiceCollection services = new();
+
+    #region View Models
+
+    services.AddSingleton<MainWindowViewModel>();
+    services.AddTransient<ContactFormViewModel>();
+
+    #endregion
+
+    Ioc.Default.ConfigureServices(services.BuildServiceProvider());
   }
 }
--- ContactApp.Wpf/ViewLocator.cs
+++ ContactApp.Wpf/ViewLocator.cs
@@ -1,26 +1,37 @@
 using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
+using CommunityToolkit.Mvvm.ComponentModel;
 using ContactApp.Wpf.ViewModels;
+using ContactApp.Wpf.ViewModels.Forms;
+using ContactApp.Wpf.Views;
+using ContactApp.Wpf.Views.Forms;
 
 namespace ContactApp.Wpf;
 
 public class ViewLocator : IDataTemplate {
-  public Control? Build(object? param) {
-    if (param is null)
-      return null;
+  private readonly Dictionary<Type, Func<Control>> _locator = new();
 
-    var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-    var type = Type.GetType(name);
+  public ViewLocator() {
+    RegisterViewFactory<MainWindowViewModel, MainWindow>();
+    RegisterViewFactory<ContactFormViewModel, ContactFormView>();
+  }
 
-    if (type != null) {
-      return (Control)Activator.CreateInstance(type)!;
-    }
+  public Control? Build(object? data) {
+    if (data is null || !_locator.TryGetValue(data.GetType(), out var factory))
+      return new TextBlock { Text = "No ViewModel provided" };
 
-    return new TextBlock { Text = "Not Found: " + name };
+    return factory?.Invoke() ?? new TextBlock { Text = $"VM Not Registered: {data.GetType()}" };
   }
 
   public bool Match(object? data) {
-    return data is ViewModelBase;
+    return data is ViewModelBase or ObservableObject;
+  }
+
+  private void RegisterViewFactory<TViewModel, TView>()
+    where TViewModel : class
+    where TView : Control {
+    _locator.Add(typeof(TViewModel), Activator.CreateInstance<TView>);
   }
 }
