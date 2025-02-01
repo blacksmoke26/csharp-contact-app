b95373e17b165d6899f87e6c00d5d96527048d7c
Junaid Atari <mj.atari@gmail.com>
2025-02-01 16:46:18 +0500

    refactor(core): globalize common namespaces

---
+++ ContactApp.Wpf/GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using directives
+
+global using System.Collections.ObjectModel;
+global using Avalonia;
+global using Avalonia.Controls;
+global using Avalonia.Interactivity;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using Microsoft.Extensions.DependencyInjection;
+global using CommunityToolkit.Mvvm.DependencyInjection;
+global using CommunityToolkit.Mvvm.Input;
+global using ContactApp.Wpf.Models;
