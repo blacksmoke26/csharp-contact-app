8efc70eda03a4ea3fa2dd40badba9fb30f026e99
Junaid Atari <mj.atari@gmail.com>
2025-02-01 16:53:58 +0500

    refactor(core): extends class, create utility methods

--- ContactApp.Wpf/ViewLocator.cs
+++ ContactApp.Wpf/ViewLocator.cs
@@ -1,37 +1,54 @@
-using System;
-using System.Collections.Generic;
-using Avalonia.Controls;
-using Avalonia.Controls.Templates;
-using CommunityToolkit.Mvvm.ComponentModel;
 using ContactApp.Wpf.ViewModels;
 using ContactApp.Wpf.ViewModels.Forms;
 using ContactApp.Wpf.Views;
 using ContactApp.Wpf.Views.Forms;
+using HanumanInstitute.MvvmDialogs.Avalonia;
 
 namespace ContactApp.Wpf;
 
-public class ViewLocator : IDataTemplate {
+public class ViewLocator : ViewLocatorBase {
   private readonly Dictionary<Type, Func<Control>> _locator = new();
+  
+  /// <inheritdoc />
+  protected override string GetViewName(object viewModel) => viewModel.GetType().FullName!.Replace("ViewModel", "View");
 
+  /// <inheritdoc />
   public ViewLocator() {
     RegisterViewFactory<MainWindowViewModel, MainWindow>();
     RegisterViewFactory<ContactFormViewModel, ContactFormView>();
   }
 
-  public Control? Build(object? data) {
+  /// <summary>
+  /// Creates a view instance against the view model 
+  /// </summary>
+  /// <param name="data">View Model instance</param>
+  /// <returns>View instance</returns>
+  public override Control Build(object? data) {
     if (data is null || !_locator.TryGetValue(data.GetType(), out var factory))
-      return new TextBlock { Text = "No ViewModel provided" };
+      return new TextBlock { Text = "ViewModel Not Found:" };
 
-    return factory?.Invoke() ?? new TextBlock { Text = $"VM Not Registered: {data.GetType()}" };
+    return factory.Invoke();
   }
-
-  public bool Match(object? data) {
+  
+  /// <summary>
+  /// Checks whatever the given object is a ViewModel instance
+  /// </summary>
+  /// <param name="data">The object</param>
+  /// <returns>True when matched, false otherwise</returns>
+  public override bool Match(object? data) {
     return data is ViewModelBase or ObservableObject;
   }
 
+  /// <summary>
+  /// Adds the ViewModel / View into the locator registry
+  /// </summary>
   private void RegisterViewFactory<TViewModel, TView>()
     where TViewModel : class
     where TView : Control {
     _locator.Add(typeof(TViewModel), Activator.CreateInstance<TView>);
   }
+
+  
+  /// <inheritdoc cref="Build"/>
+  public override object Create(object viewModel) => Build(viewModel);
 }
