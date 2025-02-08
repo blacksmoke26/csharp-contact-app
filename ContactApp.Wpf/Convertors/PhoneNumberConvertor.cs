64d50512bf3cc508de3a3b277c9398894ee3a31c
Junaid Atari <mj.atari@gmail.com>
2025-02-08 15:35:27 +0500

    refactor(code): improve convert-back value logic, implement placeholder value, update docs

--- ContactApp.Wpf/Convertors/PhoneNumberConvertor.cs
+++ ContactApp.Wpf/Convertors/PhoneNumberConvertor.cs
@@ -1,4 +1,9 @@
+// Licensed to the end users under one or more agreements.
+// Copyright (c) 2025 Junaid Atari, and contributors
+// Website: https://github.com/blacksmoke26/
+
 using System.Globalization;
+using System.Text.RegularExpressions;
 using Avalonia.Data.Converters;
 
 namespace ContactApp.Wpf.Convertors;
@@ -7,16 +12,20 @@ namespace ContactApp.Wpf.Convertors;
 /// Converts a phone number number into a readable string (+92-345-5776662) and vice-versa 
 /// </summary>
 public class PhoneNumberConvertor : IValueConverter {
+  /// <inheritdoc/>
   public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-    if (value == null) return null;
-    
+    var placeholder = parameter ?? "-";
+
+    if (value == null) return placeholder;
+
     var phone = (string)value;
     return string.Concat(phone[..3], "-", phone[3..6], "-", phone[6..]);
   }
 
+  /// <inheritdoc/>
   public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
     return value == null
-      ? string.Empty
-      : (value as string)?.Replace("-", string.Empty);
+      ? null
+      : Regex.Replace((string)value, @"/[^\+\d]+/", String.Empty);
   }
 }
