165703d043497e058e01242fc8d15b44b9fe1f48
Junaid Atari <mj.atari@gmail.com>
2025-02-08 14:10:59 +0500

    feat(view): implement scroll viewer component
    Implements: override window-resize event to calculate the scrollbar height.

--- ContactApp.Wpf/Views/MainWindow.axaml.cs
+++ ContactApp.Wpf/Views/MainWindow.axaml.cs
@@ -9,5 +9,21 @@ namespace ContactApp.Wpf.Views;
 public partial class MainWindow : UrsaWindow {
   public MainWindow() {
     InitializeComponent();
+    
+    // attached events
+    Resized += MainWindow_Resized;
+  }
+
+  /// <summary>
+  /// Event triggers when window is being resized
+  /// </summary>
+  /// <param name="sender">The sender object</param>
+  /// <param name="e">Event arguments</param>
+  private void MainWindow_Resized(object? sender, WindowResizedEventArgs e) {
+    var elm = this.FindControl<ScrollViewer>("ListingScrollViewer");
+
+    if (elm != null) {
+      elm.Height = e.ClientSize.Height - 259; // - Search input control height
+    }
   }
 }
